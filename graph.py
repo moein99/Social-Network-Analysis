3dfcaf82281d577c2ea89aa912bb8cac1f21eabb
Moein Mirzaei <moein.mirzaei95@gmail.com>
2021-11-22 23:34:28 -0700

    First method for influencers

--- graph.py
+++ graph.py
@@ -1,6 +1,9 @@
 import csv
 import itertools
+import os
 import pickle
+
+from networkx import betweenness_centrality
 from tqdm import tqdm
 import networkx as nx
 import statistics
@@ -11,9 +14,39 @@ class Graph:
     DATA_ADDRESS = "data"
     GRAPH_FILE_NAME = "graph.txt"
     JUDGEMENT_VALIDITY_LIMIT = 3  # if there are more common venues than this, judgement_validity will be 1
+    VENUE_METADATA_FIELD = "venue_ratings"
+    BC_FILE_ADDRESS = f"{DATA_ADDRESS}/bc.txt"
 
     def __init__(self):
         self.graph = self.graph = nx.Graph()
+
+    def get_average_influence_for_top_bc(self, top_bc_percentage_start, top_bc_percentage_end):
+        bc_values = [(user_id, bc_val) for user_id, bc_val in self.__get_betweenness_centrality().items()]
+        bc_values.sort(key=lambda k: k[1])
+        end = len(bc_values) - int(len(bc_values) * top_bc_percentage_start)
+        start = len(bc_values) - int(len(bc_values) * top_bc_percentage_end)
+        top_bc_users = [user_id for user_id, _ in bc_values[start:end]]
+        influences = list(map(self.__calculate_user_influence, top_bc_users))
+        return statistics.mean(influences)
+
+    def __calculate_user_influence(self, user_id):
+        total_common_venues_with_neighbors = 0
+        total_influences_on_neighbors = 0
+        for venue_id, user_rate in self.graph.nodes[user_id][self.VENUE_METADATA_FIELD].items():
+            for node in self.graph.neighbors(user_id):
+                if not self.graph.nodes[node][self.VENUE_METADATA_FIELD].get(venue_id):
+                    continue
+                total_common_venues_with_neighbors += 1
+                if int(user_rate) - 1 < int(self.graph.nodes[node][self.VENUE_METADATA_FIELD][venue_id]) < int(user_rate) + 1:
+                    total_influences_on_neighbors += 1
+        return total_influences_on_neighbors / total_common_venues_with_neighbors
+
+    def __get_betweenness_centrality(self):
+        if os.path.isfile(self.BC_FILE_ADDRESS):
+            return pickle.load(open(self.BC_FILE_ADDRESS, 'rb'))
+        results = betweenness_centrality(self.graph, normalized=True)
+        pickle.dump(results, open(self.BC_FILE_ADDRESS, 'wb'))
+        return results
 
     def set_nodes_and_edges(self):
         user_venue_ratings = self.__get_user_venue_ratings()
@@ -29,6 +62,8 @@ class Graph:
             judgement_validity = self.__get_judgement_validity(len(temp_rating_diffs))
             if len(temp_rating_diffs) > 0:
                 self.graph.add_edge(pair[0], pair[1], weight=statistics.mean(temp_rating_diffs) * judgement_validity)
+                self.graph.nodes[pair[0]][self.VENUE_METADATA_FIELD] = user_venue_ratings[pair[0]]
+                self.graph.nodes[pair[1]][self.VENUE_METADATA_FIELD] = user_venue_ratings[pair[1]]
 
     def __get_user_venue_ratings(self):
         ratings = Rating.read_ratings(f'{self.DATA_ADDRESS}/ratings.txt')
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import networkx as nx
+from networkx import betweenness_centrality
 
 from graph import Graph
 from models import clean_data
@@ -10,5 +11,7 @@ graph = Graph()
 graph.read_graph()
 print(nx.info(graph.graph))
 print(f"Average degree: {sum(d for n, d in graph.graph.degree()) / graph.graph.number_of_nodes()}")
-
+print(f"Average influence for top 5% is {graph.get_average_influence_for_top_bc(0, 0.05)}")
+print(f"Average influence for top 5-10% is {graph.get_average_influence_for_top_bc(0.05, 0.1)}")
+print(f"Average influence for top 10-15% is {graph.get_average_influence_for_top_bc(0.1, 0.15)}")
 # graph.export_graph_to_csv()
